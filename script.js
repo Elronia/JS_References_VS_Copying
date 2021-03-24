e15cd4db2330f834cd3323f03a1154640df41e00
Elronia <zar.catherine@gmail.com>
2021-03-23 23:43:07 -0400

    working with arrays

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 // start with strings, numbers and booleans
-let age = 100;
-let age2 = age;
-console.log(age, age2);
-age = 200;
-console.log(age, age2);
+// let age = 100;
+// let age2 = age;
+// console.log(age, age2);
+// age = 200;
+// console.log(age, age2);
 
-let name = 'Kate';
-let name2 = name;
-console.log(name, name2);
-name = 'katherine';
-console.log(name, name2);
+// let name = 'Kate';
+// let name2 = name;
+// console.log(name, name2);
+// name = 'katherine';
+// console.log(name, name2);
 
 // Let's say we have an array
 const players = ['Kate', 'Sarah', 'Ryan', 'Poppy'];
 
 // and we want to make a copy of it.
-
+const team = players;
+console.log(players, team);
 // You might think we can just do something like this:
+team[3] = 'Lux';
 
 // however what happens when we update that array?
 
@@ -27,12 +29,19 @@ const players = ['Kate', 'Sarah', 'Ryan', 'Poppy'];
 // Why? It's because that is an array reference, not an array copy. They both point to the same array!
 
 // So, how do we fix this? We take a copy instead!
+const team2 = players.slice();
 
 // one way
 
 // or create a new array and concat the old one in
+const team3 = [].concat(players);
 
 // or use the new ES6 Spread
+const team4 = [...players];
+team4[3] = 'hurrah!';
+console.log(team4);
+
+const team5 = Array.from(players);
 
 // now when we update it, the original one isn't changed
 
