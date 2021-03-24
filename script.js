4b859425e9414994697e7b867888e6515a0597ff
Elronia <zar.catherine@gmail.com>
2021-03-23 23:54:14 -0400

    working with objects

--- script.js
+++ script.js
@@ -54,9 +54,29 @@ const person = {
 };
 
 // and think we make a copy:
+const captain = person;
+captain.number = 99;
 
 // how do we take a copy instead?
+const cap2 = Object.assign({}, person, { number: 99, age: 12 });
+console.log(cap2);
 
 // We will hopefully soon see the object ...spread
+const cap3 = {...person};
 
 // Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+const kate = {
+    name: 'Kate',
+    age: 100,
+    social: {
+        medium: 'blog',
+        linkedin: 'profile'
+    }
+};
+
+console.clear();
+console.log(kate);
+
+const dev = Object.assign({}, kate);
+
+const dev2 = JSON.parse(JSON.stringify(kate));
